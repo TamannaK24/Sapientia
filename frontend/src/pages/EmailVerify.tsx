e9f07e746881bb769b582e3d89dc58ec11616945
tamannak24 <tamanna122404@gmail.com>
2025-08-10 19:59:49 -0500

    Add .gitignore to ignore node_modules and env files

---
+++ frontend/src/pages/EmailVerify.tsx
@@ -0,0 +1,7 @@
+export default function EmailVerify() {
+  return (
+    <div>
+      <h1 className="text-4xl">Email Verify</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/ResetPassword.tsx
@@ -0,0 +1,7 @@
+export default function ResetPassword() {
+  return (
+    <div>
+      <h1 className="text-4xl">ResetPassword</h1>
+    </div>
+  );
+}
